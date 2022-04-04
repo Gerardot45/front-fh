ee6f03a407b2881b54751ab1394f334f88f39b90
Gerardot45 <63618451+Gerardot45@users.noreply.github.com>
2022-04-04 18:49:54 -0500

    Primer commit

---
+++ src/types/types.js
@@ -0,0 +1,17 @@
+export const types = {
+  uiOpenModal: "[ui] Open modal",
+  uiCloseModal: "[ui] Close modal",
+
+  eventSetActive: "[event] Set Active",
+  eventAddNew: "[event] Add new",
+  eventClearActiveEvent: "[event] Clear active event",
+  eventUpdated: "[event] Event updated",
+  eventDeleted: "[event] Event deleted",
+
+  authChecking: "[Auth] Checking Login State",
+  authCheckingFinish: "[Auth] Finish Login State",
+  authLogin: "[Auth] Login",
+  authStartRegister: "[Auth] Start Register",
+  authStartTokenRenew: "[Auth] Start Token renew",
+  authStartLogout: "[Auth] Start Logout",
+};
